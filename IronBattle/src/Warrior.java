3eb4c7dbe5085c36ad12b0da6b9d536a9ebd59ad
Yas <yasmiineri@gmail.com>
2024-08-27 18:13:16 +0200

    first commit

---
+++ IronBattle/src/Warrior.java
@@ -0,0 +1,29 @@
+public class Warrior extends Character {
+    private int stamina;
+    private int strength;
+
+ // constructor
+ public Warrior(String name, int hp, int stamina, int strength) {
+     super(name, hp);
+     this.stamina = stamina;
+     this.strength = strength;
+ }
+
+    public int getStamina() {
+        return stamina;
+    }
+
+    public void setStamina(int stamina) {
+        this.stamina = stamina;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+}
+
+
