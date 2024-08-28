a289d53bdd6f4a6da26b070af207bd75c35e3b6b
Yas <yasmiineri@gmail.com>
2024-08-28 09:21:49 +0200

    test

---
+++ IronBattle/src/Character.java
@@ -0,0 +1,53 @@
+public class Character {
+    private String Id;
+    private String name;
+    private int hp;
+    private boolean isAlive = true;
+
+    // Constructor
+    public Character(String name, int hp) {
+        if(puntosDeVida <= 0){
+            puntosDeVida = 1;
+        }
+        this.name = name;
+        this.hp = hp;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String id) {
+        Id = id;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+        // Actualiza el estado de isAlive basado en el valor de hp
+        if(this.hp > 0) {
+            this.isAlive = true;
+        } else {
+            this.isAlive = false;
+        }
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public void setAlive(boolean alive) {
+        isAlive = alive;
+    }
+}
---
+++ IronBattle/src/Warrior.java
@@ -0,0 +1,47 @@
+import java.util.Random;
+
+public class Warrior extends Character {
+    private int stamina;
+    private int strength;
+
+    // constructor
+    public Warrior(String name, int hp, int stamina, int strength) {
+        super(name, hp);
+        this.stamina = stamina;
+        this.strength = strength;
+    }
+
+    public int getStamina() {
+        return stamina;
+    }
+
+    public void setStamina(int stamina) {
+        this.stamina = stamina;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public void attack(Character enemy) {
+        Random rand = new Random();
+        boolean heavyAttack = rand.nextBoolean(); // Decidir aleatoriamente si el ataque es pesado o débil
+        int damage;
+        if (enemy.getIsAlive()) { // Solo atacar si el enemigo está vivo
+            if (this.stamina >= 5 && heavyAttack) { // Ataque pesado si hay suficiente stamina y se decide atacar pesado
+                damage = this.strength;
+                enemy.setHp(enemy.getHp() - damage); // Aplica daño completo al enemigo
+                this.stamina -= 5;  // Consume 5 puntos de stamina
+            } else { // Ataque débil o si no hay suficiente stamina para un ataque pesado
+                damage = this.strength / 2;
+                enemy.setHp(enemy.getHp() - damage); // Aplica la mitad del daño al enemigo
+                this.stamina += 1;  // Recupera 1 punto de stamina
+            }
+        }
+    }
+// MIRAR EL METDO PARA IMPLEMENTAR EL GET IS ALIVE
+}
